939b6cc94790082068412f841a467d9832094ece
Lizeth Lopez <lizlopez.10@hotmail.com>
2023-10-05 22:40:01 -0500

    add navigation , navbar and sidebar

---
+++ src/app/[locale]/techTest/page.tsx
@@ -0,0 +1,8 @@
+export default function TechTest() {
+    return (
+      <main className="flex flex-col items-center justify-between p-24">
+        <p>tech test</p>
+      </main>
+    )
+}
+  
---
+++ src/app/components/navigation/navbar/Button.tsx
@@ -0,0 +1,7 @@
+const Button = () => {
+    return (
+      <button className="h-12 rounded-lg bg-white font-bold px-5">Perfil</button>
+    );
+  };
+  
+  export default Button;
