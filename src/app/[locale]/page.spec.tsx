2514cd30ee455d7f23ffd00312c437cb5faa251e
Viviana Rey <z.rey@uniandes.edu.co>
2023-10-13 11:13:27 -0500

    page base test change

--- src/app/[locale]/page.spec.tsx
+++ src/app/[locale]/page.spec.tsx
@@ -5,7 +5,7 @@ import Home from './page';
 describe('Home page', () => {
   it('should render content', () => {
     const { getByText } = render(<Home />);
-    const signUpMessage = getByText('Something here');
+    const signUpMessage = getByText('Hello word');
     expect(signUpMessage).toBeDefined();
   });
 });
