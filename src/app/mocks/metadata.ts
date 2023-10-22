fe79e9f112222817b76ef31dcef8015a44e8693a
Jeyson Vega <jvega@univision.net>
2023-10-21 22:51:07 -0500

    fix unit tests

---
+++ src/app/mocks/metadata.ts
@@ -0,0 +1,38 @@
+export const candidateMetadata = {
+  "habilidadesTecnicas": [
+    {
+      "id": 1,
+      "descripcion": "NodeJs"
+    },
+    {
+      "id": 2,
+      "descripcion": "Angular"
+    }
+  ],
+  "habilidadesBlandas": [
+    {
+      "id": 1,
+      "descripcion": "Comunicacion"
+    },
+    {
+      "id": 2,
+      "descripcion": "Asertividad"
+    }
+  ],
+  "idiomas": [
+    {
+      "id": 1,
+      "idioma": "Español"
+    },
+    {
+      "id": 2,
+      "idioma": "Ingles"
+    }
+  ],
+  "roles": [
+    {
+      "id": 1,
+      "rol": "Desaarrollador JS JR"
+    }
+  ]
+}
