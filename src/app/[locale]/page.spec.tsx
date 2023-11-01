bbbc709c96ca6e32c0c9f70a93d7ce6345255a8c
Viviana Rey <z.rey@uniandes.edu.co>
2023-11-01 13:58:44 -0500

    package revert changes

--- src/app/[locale]/page.spec.tsx
+++ src/app/[locale]/page.spec.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import Home from './page';
-import { labelsHome } from '../../app/mocks/labels'
 import '@testing-library/jest-dom';
 
 jest.mock('next-intl', () => ({
