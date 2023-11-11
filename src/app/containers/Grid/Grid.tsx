99bdad024dedfcc7db64bd62ae697402932fc43f
Lizeth Lopez <lizlopez.10@hotmail.com>
2023-11-11 00:17:34 -0500

    add list grid projects

---
+++ src/app/containers/Grid/Grid.tsx
@@ -0,0 +1,27 @@
+'use client'
+import React, { Suspense } from 'react';
+
+function Grid({ labels, headers, content }:any) {
+ 
+ return (
+    <Suspense fallback={<p>{labels.label_loading}</p>}>
+      {content.length === 0 && <p>{labels.label_not_test}</p>}
+      {content.length > 0 && (
+        <div className="grid grid-cols-3 bg-[#fafafb]">
+            {headers.map((header:any, index: number) => (
+                <div className='text-[#565E6C] font-bold pt-5 pb-5 pl-5 pr-5' key={index}>{header}</div>
+            ))}
+            {content.map((row:any, index: number) => (
+                <>
+                    <div className='text-[#565E6C] font-light pt-5 pb-5 pl-5 pr-5' key={index}>{row.nombre}</div>
+                    <div className='text-[#565E6C] font-light pt-5 pb-5 pl-5 pr-5' key={index}>{row.descripcion}</div>
+                    <div className='text-[#565E6C] font-light pt-5 pb-5 pl-5 pr-5' key={index}>{row.estado}</div>
+                </>
+            ))}
+        </div>
+      )}     
+    </Suspense>
+  );
+}
+
+export default Grid;
